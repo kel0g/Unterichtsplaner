ef71340f401676c81ce7dc8491be97b38ba89adb
kel0g <maulkelvin@dv-schulen.de>
2025-10-22 09:38:16 +0200

    verschiebung

--- abwesenheit.php
+++ abwesenheit.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Abwesenheit</title>
     <link rel="stylesheet" href="styles/style.css">
-    <link rel="stylesheet" href="abwesenheit.css">
+    <link rel="stylesheet" href="styles/abwesenheit.css">
 </head>
 <body>
     <div class="abwesenheit-container">
